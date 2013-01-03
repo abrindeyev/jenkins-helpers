fddae816034f80ffa4b9a76ba82e48114eaf6934
Andrey Brindeyev <abrindeyev@griddynamics.com>
2013-01-03 15:05:46 -0800

    Added urlescape function and renamed curl variable

--- bin/common.sh
+++ bin/common.sh
@@ -11,5 +11,25 @@ export PATH="$PATH:$(dirname ${BASH_SOURCE[0]})"
 
 export HttpUser="root"
 export HttpPassword=$(get_api_token.rb $HttpUser)
-export curl="/usr/bin/curl -s --basic -u $HttpUser:$HttpPassword"
+export PoolAPI="http://webapp-un.c4d.griddynamics.net"
 
+export curl="/usr/bin/curl -s"
+export jcurl="$curl --basic -u $HttpUser:$HttpPassword"
+export pcurl="$curl $PoolAPI"
+
+# http://stackoverflow.com/questions/296536/urlencode-from-a-bash-script
+urlencode() {
+  local string="${1}"
+  local strlen=${#string}
+  local encoded=""
+
+  for (( pos=0 ; pos<strlen ; pos++ )); do
+     c=${string:$pos:1}
+     case "$c" in
+        [-_.~a-zA-Z0-9] ) o="${c}" ;;
+        * )               printf -v o '%%%02x' "'$c"
+     esac
+     encoded+="${o}"
+  done
+  echo "${encoded}"    # You can either set a return variable (FASTER) 
+}
